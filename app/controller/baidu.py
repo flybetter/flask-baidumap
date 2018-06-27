60b80a88e728bd3fec01ac1781e5bd3fcd1aa75b
flybetter <flybetter@163.com>
2018-06-27 13:59:23 +0800

    finish the web html and flask. the next step to finish multi-thread and multi-process. or even scrapy

--- app/controller/baidu.py
+++ app/controller/baidu.py
@@ -9,10 +9,42 @@
 from app import app
 from flask import render_template
 from app.controller.crawl import FILE_NAME
+from app.mysql.json_data import *
+import json
 
 
 @app.route("/")
 def show():
+	datas = select_all()
 	with open(FILE_NAME, "r+") as f:
 		points = f.read()
-	return render_template("map.html", points=points)
+	return render_template("map.html", points=points, datas=datas)
+
+
+@app.route("/xiaoqu/<id>")
+def show_id(id):
+	points_list = []
+	datas = select_all()
+	xiaoqu = select_one(id)
+	xiaoqu_name_list = ''
+	one = json.loads(xiaoqu[0][2])
+	del one['name']
+	points_list.append(one)
+
+	xiaoqu_list = json.loads(xiaoqu[0][3])
+	for object in xiaoqu_list:
+		xiaoqu_name_list += object['name'] + ','
+		del object['name']
+		points_list.append(object)
+
+	points = json.dumps(points_list, ensure_ascii=False, default=house2dict)
+
+	return render_template("map.html", points=points, datas=datas, xiaoqu=xiaoqu, xiaoqu_name_list=xiaoqu_name_list)
+
+
+def house2dict(house):
+	return {
+		'lng': house.lng,
+		'lat': house.lat,
+		'count': house.count
+	}
--- app/mysql/json_data.py
+++ app/mysql/json_data.py
@@ -6,6 +6,9 @@
 @create_time= 2018/6/26 上午11:26
 """
 import pymysql.cursors
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def save(name, parent_json, children_json):
@@ -20,12 +23,44 @@ def save(name, parent_json, children_json):
 	)
 	cursor = connection.cursor()
 	sql = "insert into lianjia_json (name,parent_json,children_json) VALUES ('%s','%s','%s')" % (
-	name, parent_json, children_json)
+		name, parent_json, children_json)
 	cursor.execute(sql)
 	connection.commit()
 	cursor.close()
 	connection.close()
 
 
+def select_one(id):
+	cursor, connection = mysql_connect()
+	sql = "select * from lianjia_json where id=" + id
+	cursor.execute(sql)
+	data = cursor.fetchall()
+	logging.info(data)
+	mysql_close(cursor, connection)
+	return data
+
+
+def select_all():
+	cursor, connection = mysql_connect()
+	sql = "select * from lianjia_json"
+	cursor.execute(sql)
+	datas = cursor.fetchall()
+	mysql_close(cursor, connection)
+	return datas
+
+
+def mysql_connect():
+	connection = pymysql.connect(host='192.168.10.200', port=3306, user='root', password='', database='demo',
+								 use_unicode=True, charset='utf8')
+	cursor = connection.cursor()
+	return cursor, connection
+
+
+def mysql_close(cursor, connection):
+	connection.commit()
+	cursor.close()
+	connection.close()
+
+
 if __name__ == '__main__':
-	save()
+	select_one('1')
